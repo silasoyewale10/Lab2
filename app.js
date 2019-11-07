5d2055ca19f3a4bde6e7a726e6dfa0c0968b72fa
Silas Oyewale <silasoyewale10@gmail.com>
2019-11-07 08:33:14 -0800

    lab2 done

---
+++ app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+alert("You are welcome to the greatest website in the world and as a matter of fact, the greatest website that ever will be")
+var name = prompt("Please what is your name")
+alert(name + " , " + " you are welcome to the making of the greatest guessing site that ever will be")
+
+var coding = prompt("Does Silas like coding?");
+//coding.toLowerCase();
+
+if (coding.toLowerCase() === 'yes' || coding.toLowerCase() === 'y'){
+ // console.log("You answered "  + coding + " and you are absolutely right" );
+  alert("You answered "  + coding + " and you are absolutely right" );
+}else{
+  // console.log("You answered "  + coding + " and you are absolutely wrong");
+  alert("You answered "  + coding + " and you are absolutely wrong");
+}
+
+var instrument = prompt('Does Silas play any musical instrument?');
+//var instrument_answer = instrument.toLowerCase();
+if (instrument.toLowerCase() === 'yes' || instrument.toLowerCase() === 'y'){
+  //console.log("You answered "  + instrument + " and you are absolutely wrong");
+  alert("You answered "  + instrument + " and you are absolutely wrong");
+}else{
+  // console.log("You answered "  + instrument + " and you are absolutely right");
+  alert("You answered "  + instrument + " and you are absolutely right");
+
+}
+
+var house = prompt('Does Silas have a house?');
+//house.toLowerCase();
+if (house.toLowerCase() ==='yes' || house.toLowerCase() === 'y'){
+  //console.log("You answered " + house + " and you are absolutely right");
+  alert("You answered " + house + " and you are absolutely right");
+}else{
+  //console.log('You answered '  + house + " and you are absolutely wrong");
+  alert('You answered '  + house + " and you are absolutely wrong");
+
+}
+
+var commute = prompt('Does Silas drive to Code Fellows?');
+//commute.toLowerCase();
+if (commute.toLowerCase() ==='yes' || commute.toLowerCase === 'y'){
+  //console.log("You answered " + commute + " and you are absolutely wrong");
+  alert("You answered " + commute + " and you are absolutely wrong");
+
+}else{
+  //console.log('You answered '  + commute + " and you are absolutely right");
+  alert('You answered '  + commute + " and you are absolutely right");
+
+}
+
+var phobia = prompt('Does Silas have phobia for rattle snakes?');
+//phobia.toLowerCase();
+if (phobia.toLowerCase() ==='yes' || phobia.toLowerCase() === 'y'){
+  //console.log("You answered "  + phobia + " and you are absolutely right");
+  alert("You answered "  + phobia + " and you are absolutely right");
+
+}else{
+  //console.log("You answered "  + phobia + " and you are absolutely wrong");
+  alert("You answered "  + phobia + " and you are absolutely wrong");
+}
+
+alert(name + " , " + " thank you for exploring the greatest guessing site that ever will be")
+
+
